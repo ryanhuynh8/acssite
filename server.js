4f79527dc6de13780abc5085f7378e45f76b706c
yuhuynh <yuhuynh@gmail.com>
2015-05-16 15:45:56 +0000

    change directory structure

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
