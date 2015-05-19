4e31883c71b06db3818de076b7b7671570b7e0ab
yuhuynh <yuhuynh@gmail.com>
2015-05-19 15:45:29 +0000

    add template theme

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 var Models = require("./models/models");
 
-//Models.initModels();
 var Task = Models.Task;
 var User = Models.User;
 
-app.get('/', function(req, res) {
-  // Task.findAll({
-  //   include: [{
-  //     model: User,
-  //     attributes: ['email']
-  //   }],
-  //   where: {
-  //     readed: false
-  //   }
-  // }).then(function(result) {
-  //   res.send(JSON.stringify(result));
-  //   res.end();
-  // });
-  var tasks = Task.findByUserCreated(69);
-  res.send(JSON.stringify(tasks));
-  res.send(Task.Foo);
-  res.end();
+// Task.findAll({
+//   include: [{
+//     model: User,
+//     attributes: ['email']
+//   }],
+//   where: {
+//     readed: false
+//   }
+
+router.use(function (req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
 });
+
+// get all task
+router.get('/task/incomplete', function(req, res) {
+  Task.findAll({
+    where:{
+      status_task_id: 19
+    }
+  })
+  .then(function (tasks) {
+    res.json(tasks);
+    res.end();
+  })
+})
+
+app.use('/api', router);
+app.use(express.static('web'));
 
 app.listen(8080);
 console.log('Express started on port 8080');
