adafe1006bda8d0081c5c672062da783d5627d73
yuhuynh <yuhuynh@gmail.com>
2015-06-28 09:23:32 +0000

    commit from server-side code

--- api.js
+++ api.js
@@ -10,35 +10,17 @@ function auth_require(req, res, roles) {
   // if (req.session.user_id == null)
   // {
   //   res.json({message: "Access denied"});
-  //   res.status(404);
+  //   res.status(401);
   //   res.end();
   // }
   // else
   return true;
 }
 
-// LIST ALL INCOMPLETED TASK: /api/task/incompleted
-router.get('/task/incompleted', function(req, res) {
-  if (!auth_require(req, res, 'admin')) return;
-
-  Task.findAll({
-      include: {
-        as: 'assignee',
-        attributes: ['first_name', 'last_name']
-      },
-      where: {
-        status_task_id: 19
-      }
-    })
-    .then(function(tasks) {
-      res.json(tasks);
-      res.end();
-    });
-});
-
-// LIST TASKS FOR CURRENT LOGGED IN USER: /api/task/list
+// LIST TASKS FOR CURRENT LOGGED IN USER
 router.get('/task/list', function(req, res)   {
   if (!auth_require(req, res, 'admin')) return;
+
   var user_id = req.session.user_id;
   Task.findAll({
     include: [{
@@ -46,7 +28,7 @@ router.get('/task/list', function(req, res)   {
       as: 'assignee',
       attributes: ['id'],
       where: {
-        'id': user_id
+        id: user_id
       }
     }, {
       model: User,
@@ -54,9 +36,9 @@ router.get('/task/list', function(req, res)   {
       attributes: ['first_name', 'last_name']
     }],
     where: {
-      // status_task_id: 19
+        status_task_id: 19
     },
-    order: 'due_date DESC',
+    order: 'create_on DESC',
     limit : 100
   }).then(function(tasks) {
     res.json(tasks);
@@ -64,8 +46,36 @@ router.get('/task/list', function(req, res)   {
   });
 });
 
-// LIST ALL TASK: /api/task/list/all
-router.get('/task/list/all', function(req, res) {
+// LIST ARCHIVED TASKS FOR CURRENT LOGGED IN USER
+router.get('/task/list/archived', function(req, res)   {
+  if (!auth_require(req, res, 'admin')) return;
+
+  var user_id = req.session.user_id;
+  Task.findAll({
+    include: [{
+      model: User,
+      as: 'assignee',
+      attributes: ['id'],
+      where: {
+        id: user_id
+      }
+    }, {
+      model: User,
+      as: 'poster',
+      attributes: ['first_name', 'last_name']
+    }],
+    where: {
+        status_task_id: 21
+    },
+    order: 'create_on DESC',
+  }).then(function(tasks) {
+    res.json(tasks);
+    res.end();
+  });
+});
+
+// LIST ALL TASK
+router.get('/task/all/list', function(req, res) {
   if (!auth_require(req, res, 'admin')) return;
 
   Task.findAll({
@@ -85,25 +95,52 @@ router.get('/task/list/all', function(req, res) {
   });
 });
 
+// LIST ALL INCOMPLETED TASK
+router.get('/task/all/archived', function(req, res) {
+  if (!auth_require(req, res, 'admin')) return;
+
+  Task.findAll({
+    include: [{
+      model: User,
+      as: 'assignee',
+      attributes: ['first_name', 'last_name']
+    }, {
+      model: User,
+      as: 'poster',
+      attributes: ['first_name', 'last_name']
+    }],
+    where: {
+      status_task_id: 21
+    },
+    order: [['create_on','DESC']]
+  }).then(function(tasks) {
+    res.json(tasks);
+    res.end();
+  });
+});
+
 // CREATE A NEW TASK
 router.post('/task/new', function(req, res) {
     if (!auth_require(req, res, 'admin')) return;
-    
+
     var task = req.body;
     // is the data typeof Task?
     if ((task.due_date === undefined) || (task.task_description === undefined) || (task.assign_by === undefined))
     {
+      res.status(400);
       res.json({ message: 'invalid' });
       res.end();
       return;
     }
-    
+
     task.posted_by = req.session.user_id;
     Task.create(task)
       .then(function(result){
+        res.status(201);
         res.json({ message: 'success' })
       })
       .catch(function(err) {
+        res.status(503);
         res.json({ message: err });
         console.log(err);
       })
@@ -112,10 +149,158 @@ router.post('/task/new', function(req, res) {
       })
 })
 
-// SEARCH FOR TASKS BASE ON VARIOUS CRITERIAS
-router.get('task/search', function(req, res) {
+// GENERIC SEARCH FUNCTION
+var searchTask = function(req, res, search_params) {
+  
+  // semi- query building
+  var opt_task = {};
+  var opt_assigned_by = {};
+  var opt_status = {};
+  var opt_id = {};
+  
+  if (search_params.user_id === undefined)
+    opt_id = { ne: 'NULL' };
+  else
+    opt_id = { like: search_params.user_id };
+
+  if (search_params.assignee === undefined)
+    opt_id = { ne: 'NULL' };
+  else
+    opt_id = { like : search_params.assignee };
     
-})
+  if (search_params.task_description === undefined)
+    opt_task = { ne: 'NULL' };
+  else
+    opt_task = { like : '%'+ search_params.task_description +'%' };
+
+  if (search_params.assigned_by === undefined)
+    opt_assigned_by = { ne: 'NULL' };
+  else
+    opt_assigned_by = { like : search_params.assigned_by };
+    
+  if (search_params.status === undefined)
+    opt_status = { ne: 'NULL' };
+  else
+    opt_status = { like: search_params.status };
+  
+  Task.findAll({
+    include: [
+      {
+        model: User,
+        as: 'assignee',
+        attributes: ['id', 'first_name', 'last_name'],
+        where: {
+          id: opt_id
+        }
+      },
+      {
+        model: User,
+        as: 'poster',
+        attributes: ['first_name', 'last_name'],
+        where: {
+          id: opt_assigned_by
+        }
+      }
+    ],
+    where: {
+      task_description: opt_task,
+      status_task_id: opt_status
+    },
+    order: 'due_date DESC',
+    limit: 100
+  })
+  .then(function(tasks) {
+    res.status(200);
+    res.json(tasks);
+  })
+  .catch(function(err){
+    console.log(err);
+    res.status(500);
+  })
+  .finally(function(){
+    res.end();
+  });
+};
+
+// SEARCH FOR TASKS BASE ON VARIOUS CRITERIAS
+router.post('/task/search', function(req, res) {
+  if (!auth_require(req, res, 'all')) return;
+
+  var user_id = req.session.user_id;
+  var search_params = req.body;
+
+  search_params.user_id = user_id;
+  searchTask(req, res, search_params);
+});
+
+// SEARCH FOR TASKS BASE ON VARIOUS CRITERIAS
+router.post('/task/all/search', function(req, res) {
+  if (!auth_require(req, res, 'all')) return;
+
+  var search_params = req.body;
+  searchTask(req, res, search_params);
+});
+
+// MARK TASK AS READED
+router.post('/task/readed', function(req, res) {
+  if (!auth_require(req, res, 'admin')) return;
+  // TODO: confirm task's owner or admin
+
+  var task_to_update = req.body;
+  Task.update({
+      readed: true
+    }, {
+      where: {
+        id: task_to_update.id
+      }
+    })
+    .then(function(result) {
+      res.status(200);
+      res.json({
+        message: 'success'
+      });
+    })
+    .catch(function(err) {
+      res.status(500);
+      res.json({
+        message: err
+      });
+    })
+    .finally(function(result) {
+      res.end();
+    });
+});
+
+// ARCHIVE A TASK (ie. set status to 19)
+router.post('/task/archive', function(req, res) {
+  if (!auth_require(req, res, 'admin')) return;
+  // TODO: confirm task's owner or admin
+
+  var task_to_update = req.body;
+  Task.update({
+      status_task_id: 21,
+      readed: true
+    }, {
+      where: {
+        id: task_to_update.id
+      }
+    })
+    .then(function(result) {
+      res.status(200);
+      res.json({
+        message: 'success'
+      });
+    })
+    .catch(function(err) {
+      res.status(500);
+      res.json({
+        message: err
+      });
+    })
+    .finally(function(result) {
+      res.end();
+    });
+});
 
 // UPDATE TASK DETAILS: /api/task/update
 router.post('/task/update', function(req, res) {
@@ -182,11 +367,9 @@ router.post('/auth', function(req, res) {
     })
     .then(function(user) {
       if (!user) {
-        res.json({
-          message: 'unauthorized'
-        });
-        return;
+        throw new Error('INVALID_LOGIN');
       }
+
       var md5Hash = crypto.createHash('md5');
       md5Hash.update(req.body.password);
       var hashed = md5Hash.digest('hex');
@@ -203,15 +386,26 @@ router.post('/auth', function(req, res) {
         });
       }
       else {
-        res.json({
-          message: 'unauthorized'
-        });
-        res.end();
+        throw new Error('INVALID_LOGIN');
       }
+    })
+    .catch(function (error) {
+      console.log('FAILED', error);
+      if (error.message === 'INVALID_LOGIN')
+      {
+        res.status(401).send('invalid');
+      }
+      else
+        res.status(503).send('database error');  // causing race condition
+      res.end();
+
+    })
+    .finally(function () {
+      // res.end();
     });
 });
 
-
+// LIST ALL ANNOUNCEMENT BEFORE THE EXPIRING DATE
 router.get('/announcement/list', function(req, res) {
   if (!auth_require(req, res, 'admin')) return;
 
@@ -226,6 +420,39 @@ router.get('/announcement/list', function(req, res) {
     res.json(list);
     res.end();
   });
+});
+
+// CREATE A NEW ANNOUNCEMENT
+router.post('/announcement/new', function(req, res) {
+    if (!auth_require(req, res, 'admin')) return;
+
+    var model = req.body;
+    // is the data typeof Announcement?
+    if ((model.expired_date === undefined) || (model.task_description === undefined))
+    {
+      res.status(400);
+      res.json({ message: 'invalid' });
+      res.end();
+      return;
+    }
+
+    var new_announcement = {};
+    new_announcement.expired_date = model.expired_date;
+    new_announcement.announcements_description = model.task_description;
+
+    Announcement.create(new_announcement)
+      .then(function(result){
+        res.status(201);
+        res.json({ message: 'success' });
+      })
+      .catch(function(err) {
+        res.status(503);
+        res.json({ message: err });
+        console.log(err);
+      })
+      .finally(function() {
+        res.end();
+      });
 });
 
 exports = module.exports = router;
--- test/test.js
+++ test/test.js
@@ -1,13 +1,52 @@
 var should = require('chai').should(),
     supertest = require('supertest'),
-    api = supertest('http://localhost:8080');
+    request = supertest('http://acsdemo-yuhuynh.c9.io/api');
 
-describe('POST /auth', function() {
-  it('should display error if login information is wrong', function(done) {
-    api
-      .post('/api/')
-      .set('user')
-  });
+describe('GET /user/list', function() {
+  it('should load a list of user', function (done) {
+    request.get('/user/list')
+      .end(function (err, res) {
+        res.text.should.not.be.empty;
+        console.log(res.body.length);
+        done(err);
+      })
+  })  
 });
 
+describe('POST /auth', function() {
+  it('should display error if login information is wrong', function (done) {
+    request.post('/auth')
+      .send({ user_name: 'sillyusername'})
+      .send({ password: 'sillypassword' })
+      .end(function (err, res) {
+          res.status.should.equal(401);
+          res.text.should.include('invalid');
+          done();
+      });
+  })
+  
+  it('should logged us in with correct login credential', function (done) {
+    request.post('/auth')
+      .send({ user_name: 'mike'})
+      .send({ password: '123' })
+      .end(function(err, res) {
+          res.status.should.equal(200);
+          res.text.should.include('authorized');
+          console.log(res.headers['set-cookie']);
+          done(err);
+      })
+  })
+});
 
+describe('POST /task/new', function() {
+  it('should login and retrieve a session id', function(done) {
+    request.post('/auth')
+      .send({ user_name: 'mike'})
+      .send({ password: '123' })
+      .end(function(err, res) {
+        console.log(res.headers['set-cookie']);
+        done(err);
+      })
+  })
+});
+  
